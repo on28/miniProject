aba6e52fb81ff1a44855673c533240c83facbe6d
Oliver <on28@njit.edu>
2018-09-29 21:59:33 -0400

    Feature:All classes added (in progress)

--- public/index.php
+++ public/index.php
@@ -6,5 +6,41 @@
  * Time: 6:28 PM
  */
 
-echo 'test1';
-echo 'test2';
+main::start();
+
+class main {
+
+    static public function start() {
+        $records = csv::getRecords();
+        $table = html::generateTable($records);
+        system::printPage($table);
+    }
+}
+
+class csv {
+
+    static public function getRecords() {
+
+        $records = 'test';
+    return $records;
+    }
+}
+
+class html {
+
+    static public function generateTable($records) {
+
+        $table = $records;
+
+        return $table;
+    }
+}
+
+class system {
+
+    static public function printPage($page) {
+
+        echo $page;
+    }
+
+}

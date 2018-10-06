313c926e6e46e5fdf085bdb5a1ab1afe7e2fbdcb
Oliver <on28@njit.edu>
2018-10-06 15:17:06 -0400

    Feature:Added css to the code and updated the html class

--- public/index.php
+++ public/index.php
@@ -6,21 +6,68 @@ static public function start($filename) {
     }
 }
 
-class html {
+class html
+{
     public static function generateTable($records) {
-echo "<html><body><table>\n\n";
-    $f = fopen("example.csv", "r");
-    while (($records = fgetcsv($f)) !== false) {
-    echo "<tr>";
-        foreach ($records as $cell) {
-        echo "<td>" . htmlspecialchars($cell) . "</td>";
+
+$css = <<<CSS
+<style type="text/css">
+    .tg  {border-collapse:collapse;border-spacing:0;}
+    .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
+    .tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
+    .tg .tg-0lax{text-align:left;vertical-align:top}
+</style>
+CSS;
+
+
+    $first = true;
+
+    echo $css;
+
+    echo "<table class=\"tg\">" . PHP_EOL;
+
+    foreach ($records as $record) {
+
+
+        if ($first) {
+
+            $fields = array_keys($record);
+
+            self::generateRow($fields, true);
+            $first = false;
+    }
+    $fields = array_values($record);
+
+    self::generateRow($fields);
+
+    }
+
+    echo "</table" . PHP_EOL;
+
         }
-        echo "</tr>\n";
+
+
+        public static function generateRow(array $fields, $header = false){
+
+            echo "<tr>" . PHP_EOL;
+
+            $o = "<td \"tg-0lax\">";
+            $c = "</td>";
+
+
+            if ($header){
+            $o = "<th \"tg-0lax\">";
+            $c = "</th>";
+            }
+            foreach ($fields as $field){
+            echo $o . $field . $c . PHP_EOL;
+            }
+            echo "</tr>" . PHP_EOL;
+
     }
-    fclose($f);
-    echo "\n</table></body></html>";
-    }
+
 }
+
 
 class csv {
     static public function getRecords($filename) {
